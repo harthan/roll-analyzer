322c643f5ff3726382280c846b09d66548cbad85
jacob <binarcode.eu@gmail.com>
2022-10-30 02:15:09 +0200

    initial commit

---
+++ src/main/java/eu/binarcode/rollanalyzer/RollAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package eu.binarcode.rollanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/binarcode/rollanalyzer/RollAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.binarcode.rollanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
