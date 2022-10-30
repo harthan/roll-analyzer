af8c60e7266fac4fdd94adfccab5e8b27d8d68b5
jacob <binarcode.eu@gmail.com>
2022-10-30 02:19:08 +0200

    added a component scan on root dir

--- src/main/java/eu/binarcode/rollanalyzer/RollAnalyzerApplication.java
+++ src/main/java/eu/binarcode/rollanalyzer/RollAnalyzerApplication.java
@@ -2,8 +2,10 @@ package eu.binarcode.rollanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "eu.binarcode.rollanalyzer")
 public class RollAnalyzerApplication {
 
 	public static void main(String[] args) {
